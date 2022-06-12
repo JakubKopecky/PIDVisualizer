9548606273fa64977e4597e680379cb171cc8ee4
Jakub Kopecky <dede.kopecky@gmail.com>
2022-06-12 11:17:11 +0200

    db test

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+
+	_ "modernc.org/sqlite"
+)
+
 func main() {
-	callApi()
+	//callApi()
+
+	db, err := sql.Open("sqlite", "./names.db")
+	checkErr(err)
+	defer db.Close()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
