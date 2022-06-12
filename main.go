5a3180d9d070d56d38370f0d975e3265bb835db6
Jakub Kopecky <dede.kopecky@gmail.com>
2022-06-12 12:08:08 +0200

    another db lib

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"database/sql"
 	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	callApi()
 
-	db, err := sql.Open("sqlite", "./names.db")
+	db, err := sql.Open("sqlite3", "./names.db")
 	checkErr(err)
 	defer db.Close()
 }
